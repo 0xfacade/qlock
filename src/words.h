54939c55d50ae7aaa8ce339ae827f3388f977651
Florian Behrens <florian.behrens@rocketmail.com>
2019-12-25 18:03:30 +0100

    Implement displaying the time if it is known

---
+++ src/words.h
@@ -0,0 +1,29 @@
+#include <array>
+
+using namespace std;
+
+const array<int, 5> ES_IST = {0, 19, 39, 40, 59};
+const array<int, 3> VOR = {3, 16, 23};
+const array<int, 4> NACH = {76, 83, 96, 103};
+const array<int, 3> UHR = {89, 90, 109};
+
+// Minuten
+const array<int, 4> M_FUENF = {79, 80, 99, 100};
+const array<int, 4> M_ZEHN = {1, 18, 21, 38};
+const array<int, 7> M_VIERTEL = {42, 57, 62, 77, 82, 97, 102};
+const array<int, 7> M_ZWANZIG = {41, 58, 61, 78, 81, 98, 101};
+const array<int, 4> M_HALB = {4, 15, 24, 35};
+
+// Stunden
+const array<int, 4> H_EINS = {5, 14, 25, 34};
+const array<int, 4> H_ZWEI = {74, 85, 94, 105};
+const array<int, 4> H_DREI = {6, 13, 26, 33};
+const array<int, 4> H_VIER = {73, 86, 93, 106};
+const array<int, 4> H_FUENF = {75, 84, 95, 104};
+const array<int, 5> H_SECHS = {7, 12, 27, 32, 47};
+const array<int, 6> H_SIEBEN = {8, 11, 28, 31, 48, 51};
+const array<int, 4> H_ACHT = {72, 87, 92, 107};
+const array<int, 4> H_NEUN = {30, 49, 50, 69};
+const array<int, 4> H_ZEHN = {9, 10, 29, 30};
+const array<int, 3> H_ELF = {55, 64, 75};
+const array<int, 5> H_ZWOELF = {68, 71, 88, 91, 108};
